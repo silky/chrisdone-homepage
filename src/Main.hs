340b89d09dd0386a27586da626c68be5143857eb
Chris Done <chrisdone@gmail.com>
2012-12-25 15:43:28 +0100

    Add tags and feed.

--- src/Main.hs
+++ src/Main.hs
@@ -29,6 +29,7 @@ main = hakyll $ do
     create "all-posts" $ constA mempty
         >>> arr (setField "title" "All posts")
         >>> requireAllA "posts/*" (id *** arr dateOrdered >>> addPostList)
+        >>> renderTagsField "prettytags" (fromCapture "tags/*")
         >>> applyTemplateCompiler "templates/posts.html"
         >>> applyTemplateCompiler "templates/default.html"
         >>> relativizeUrlsCompiler
@@ -37,13 +38,40 @@ main = hakyll $ do
     match "index" $ route $ idRoute
     create "index" $ constA mempty
         >>> arr (setField "title" "Home")
+        >>> requireA "tags" (setFieldA "tagcloud" (renderTagCloud'))
         >>> requireAllA "posts/*" (id *** arr (take 3 . reverse . dateOrdered) >>> addPostList)
         >>> applyTemplateCompiler "templates/index.html"
         >>> applyTemplateCompiler "templates/default.html"
         >>> relativizeUrlsCompiler
 
-    -- Read tempplates
+    -- Tags
+    create "tags" $
+        requireAll "posts/*" (\_ ps -> readTags ps :: Tags String)
+
+    -- Add a tag list compiler for every tag
+    match "tags/*" $ route $ setExtension ""
+    metaCompile $ require_ "tags"
+        >>> arr tagsMap
+        >>> arr (map (\(t, p) -> (tagIdentifier t, makeTagList t p)))
+
+    -- Render RSS feed
+    match "rss.xml" $ route idRoute
+    create "rss.xml" $
+        requireAll_ "posts/*"
+            >>> arr dateOrdered
+            >>> arr reverse
+            >>> renderRss feedConfiguration
+
+    -- Read templates
     match "templates/*" $ compile templateCompiler
+
+  where
+    renderTagCloud' :: Compiler (Tags String) String
+    renderTagCloud' = renderTagCloud tagIdentifier 100 300
+
+    tagIdentifier :: String -> Identifier (Page String)
+    tagIdentifier = fromCapture "tags/*"
+
 
 -- | Sort pages by their date field.
 dateOrdered :: [Page a] -> [Page a]
@@ -58,3 +86,24 @@ addPostList = setFieldA "posts" $
         >>> require "templates/postitem.html" (\p t -> map (applyTemplate t) p)
         >>> arr mconcat
         >>> arr pageBody
+
+-- | Make a list of posts given a tag.
+makeTagList :: String
+            -> [Page String]
+            -> Compiler () (Page String)
+makeTagList tag posts =
+    constA (mempty, posts)
+        >>> addPostList
+        >>> arr (setField "title" ("Posts tagged &#8216;" ++ tag ++ "&#8217;"))
+        >>> applyTemplateCompiler "templates/posts.html"
+        >>> applyTemplateCompiler "templates/default.html"
+        >>> relativizeUrlsCompiler
+
+feedConfiguration :: FeedConfiguration
+feedConfiguration = FeedConfiguration
+    { feedTitle       = "Chris Done's home page feed."
+    , feedDescription = "Chris Done's home page feed."
+    , feedAuthorName  = "Chris Done"
+    , feedAuthorEmail = "chrisdone@gmail.com"
+    , feedRoot        = "http://chrisdone.com"
+    }
